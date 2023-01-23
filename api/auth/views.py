96e25b4c74bff82ef66d603ca0d67cbb17a73a7d
spartan124 <ojonuba93@gmail.com>
2023-01-23 22:39:33 +0100

    user login and signup routes created

--- api/auth/views.py
+++ api/auth/views.py
@@ -2,7 +2,21 @@ from flask_restx import Namespace, Resource
 
 auth_namespace = Namespace("auth", description="namespace for authentication")
 
-@auth_namespace.route("/")
-class HelloAuth(Resource):
-    def get(self):
-        return {"message": "Hello Auth, nice to meet you"}
+@auth_namespace.route("/signup")
+class Signup(Resource):
+    def post(self):
+        """
+         Sign up a User
+        """
+        pass
+
+
+@auth_namespace.route('/login')
+class Login(Resource):
+    def post(self):
+        """
+        Login a User
+        """
+        pass
+    
+        
