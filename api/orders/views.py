748b57b2763ec76e0de2fa88a42bbf2eaf588b7d
spartan124 <ojonuba93@gmail.com>
2023-01-23 22:40:28 +0100

    orders endpoints routes created

--- api/orders/views.py
+++ api/orders/views.py
@@ -1,8 +1,65 @@
 from flask_restx import Namespace, Resource
 
-order_namespace = Namespace("order", description="namespace for orders")
+order_namespace = Namespace("orders", description="namespace for orders")
 
-@order_namespace.route("/")
-class HelloOrder(Resource):
+
+@order_namespace.route("/orders")
+class OrderGetCreate(Resource):
     def get(self):
-        return {"message": "Hello Order, nice to meet you"}
+        """
+        Get all orders
+        """
+        pass
+
+    def post(self):
+        """
+        Place an order
+        """
+        pass
+
+@order_namespace.route('/order/<int:order_id>')
+class GetUpdateDelete(Resource):
+
+    def get(self, order_id):
+        """
+            Retrieving an order by ID
+        """
+        pass
+
+    def put(self, order_id):
+        """
+           Update an Order by ID
+        """
+        pass
+
+    def delete(self, order_id):
+        """
+           Delete an Order by ID
+        """
+        pass
+
+@order_namespace.route('/user/<int:user_id>/order/<int:order_id>')
+class GetSpecificOrderByUser(Resource):
+    def get(self, user_id, order_id):
+        """
+           Get an order from a specific User
+        """
+        pass
+
+@order_namespace.route('/user/<int:user_id>/orders')
+class GetUserOrder(Resource):
+    def get(self):
+        
+        """
+           Get all Orders by user
+        """
+        pass
+
+@order_namespace.route('/order/status/<int:order_id>')
+class UpdateOrdersStatus(Resource):
+    def patch(self, order_id):
+        """
+           Update an Order's status
+        """
+        pass
+    
