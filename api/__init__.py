a0f8da8a8b6e42ac5e61834bc1b6485943c64a92
spartan124 <ojonuba93@gmail.com>
2023-01-23 22:42:11 +0100

    create_app function updated

--- api/__init__.py
+++ api/__init__.py
@@ -2,9 +2,12 @@ from flask import Flask
 from flask_restx import Api
 from .orders.views import order_namespace as ons
 from .auth.views import auth_namespace as ans
-def create_app():
+from .config.config import config_dict
+
+def create_app(config=config_dict['dev']):
     app = Flask(__name__)
 
+    app.config.from_object(config)
     api = Api(app)
 
     api.add_namespace(ons, path="")
