e61461505624e2759ca74604627143c97ccc1389
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-01-24 15:30:42 +0100

    updstes

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,19 +1,38 @@
+from http import HTTPStatus
+
 from flask import request
 from flask_restx import Namespace, Resource, fields
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from ..models.users import User
 from ..utils import db
-from werkzeug.security import generate_password_hash, check_password_hash
-from http import HTTPStatus
+
 auth_namespace = Namespace("auth", description="namespace for authentication")
 
 signup_model = auth_namespace.model(
-    'user', {
-        'id': fields.Integer(),
-        'username': fields.String(required=True, description='A username'),
-        'email' : fields.String(required=True, description='An email'),
-        'password': fields.String(required=True, description='A password')
-    }
+    "SignUp",
+    {
+        "id": fields.Integer(),
+        "username": fields.String(required=True, description="A username"),
+        "email": fields.String(required=True, description="An email"),
+        "password": fields.String(required=True, description="A password"),
+    },
 )
+
+user_model = auth_namespace.model(
+    "User",
+    {
+        # 'id': fields.Integer(),
+        "username": fields.String(required=True, description="A username"),
+        "email": fields.String(required=True, description="An email"),
+        "password_hash": fields.String(required=True, description="A password"),
+        "is_active": fields.Boolean(
+            description="This shows if a user is active or not"
+        ),
+        "is_staff": fields.Boolean(description="This shows if a user is active or not"),
+    },
+)
+
 
 @auth_namespace.route("/signup")
 class Signup(Resource):
@@ -21,28 +40,25 @@ class Signup(Resource):
     @auth_namespace.marshal_with(signup_model)
     def post(self):
         """
-         Sign up a User
+        Sign up a User
         """
         data = request.get_json()
 
         new_user = User(
-            username = data.get('username'),
-            email = data.get('email'),
-            password = generate_password_hash(data.get('password'))
+            username=data.get("username"),
+            email=data.get("email"),
+            password_hash=generate_password_hash(data.get("password")),
         )
 
         new_user.save()
 
         return new_user, HTTPStatus.CREATED
-        
 
 
-@auth_namespace.route('/login')
+@auth_namespace.route("/login")
 class Login(Resource):
     def post(self):
         """
         Login a User
         """
         pass
-    
-        
