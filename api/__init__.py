093986a306603097cfb49c45740804a0ef586c8c
spartan124 <ojonuba93@gmail.com>
2023-01-24 02:57:15 +0100

    app __init__ updates

--- api/__init__.py
+++ api/__init__.py
@@ -3,14 +3,31 @@ from flask_restx import Api
 from .orders.views import order_namespace as ons
 from .auth.views import auth_namespace as ans
 from .config.config import config_dict
+from .utils import db
+from .models.orders import Order
+from .models.users import User
+from flask_migrate import Migrate
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
 
     app.config.from_object(config)
+    
+    db.init_app(app)
+
+    migrate = Migrate(app, db)
     api = Api(app)
 
     api.add_namespace(ons, path="")
     api.add_namespace(ans, path="/auth")
+
+    @app.shell_context_processor
+    def make_shell_context():
+        return {
+            'db': db,
+            'User': User,
+            'Order': Order
+
+        }
 
     return app
