d1860e3ab7e3517bb1999c9de129d33204ca4d0d
spartan124 <ojonuba93@gmail.com>
2023-01-24 02:28:13 +0100

    order model update

---
+++ api/models/orders.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from enum import Enum
+
+from ..utils import db
+
+
+class Sizes:
+    SMALL = "small"
+    MEDIUM = "medium"
+    LARGE = "large"
+    EXTRA_LARGE = "extra_large"
+
+
+class OrderStatus:
+    PENDING = "pending"
+    IN_TRANSIT = "in_transit"
+    DELIVERED = "delivered"
+
+
+class Order(db.Model):
+    __tablename__ = "orders"
+    id = db.Column(db.Integer(), primary_key=True)
+    size = db.Column(db.Enum(Sizes), default=Sizes.SMALL)
+    order_status = db.Column(db.Enum(OrderStatus), default=OrderStatus.PENDING)
+    flavour = db.Column(db.String(), nullable=False)
+    date_created = db.Column(db.DateTime(), default=datetime.utcnow)
+    user = db.Column(db.Integer(), db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f"<Order {self.id}>"
