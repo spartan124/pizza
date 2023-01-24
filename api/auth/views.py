a6848b8a1adb35bac4e210ee20eb0bd8dd6d2bb1
spartan124 <ojonuba93@gmail.com>
2023-01-24 03:31:24 +0100

    updated user auth

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,9 +1,19 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 
 auth_namespace = Namespace("auth", description="namespace for authentication")
 
+auth_model = auth_namespace.model(
+    'user', {
+        'id': fields.Integer(),
+        'username': fields.String(required=True, description='A username'),
+        'email' : fields.String(required=True, description='An email'),
+        'password': fields.String(required=True, description='A password')
+    }
+)
+
 @auth_namespace.route("/signup")
 class Signup(Resource):
+    @auth_namespace.expect()
     def post(self):
         """
          Sign up a User
