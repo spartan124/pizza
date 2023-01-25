71eed095b2280ec83093114d86e7131ab9bad90b
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-01-25 02:28:37 +0100

    updated config with jwt config

--- api/config/config.py
+++ api/config/config.py
@@ -1,15 +1,18 @@
 import os
 from decouple import config
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 class Config:
     SECRET_KEY = config('SECRET_KEY', 'secret')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=30)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(hours=3)
+    JWT_SECRET_KEY = config('JWT_SECRET_KEY')
 
 class DevConfig(Config):
     DEBUG = config('DEBUG', cast=bool)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'+os.path.join(BASE_DIR, 'db.sqlite3')
 
