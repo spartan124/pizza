f14cf53a513b9e2bbe42e27ee9d7b311bfef2278
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-02-01 23:06:00 +0100

    flask_jwt_extended errorhandling improved

--- api/__init__.py
+++ api/__init__.py
@@ -1,32 +1,45 @@
-from flask import Flask, jsonify
+from flask import Flask
+from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 from flask_restx import Api
-from .orders.views import order_namespace as ons
+
 from .auth.views import auth_namespace as ans
 from .config.config import config_dict
-from .utils import db
 from .models.orders import Order
 from .models.users import User
-from flask_migrate import Migrate
-from flask_jwt_extended import JWTManager
+from .orders.views import order_namespace as ons
+from .utils import db
+from flask_jwt_extended.exceptions import JWTExtendedException
+import pathlib
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
 
     app.config.from_object(config)
+    app.config.from_pyfile(pathlib.Path(__file__).parent / "config/config.py")
+
     
     db.init_app(app)
-
-    jwt = JWTManager(app)
-
     
+    api = Api(app)
+
+    jwt = JWTManager()
+    
+    @api.errorhandler(JWTExtendedException)
+    def handle_jwt_exceptions(error):
+        return {'message': str(error)}, getattr(error, 'code', 401)
+    
+    jwt.init_app(app)
 
     migrate = Migrate(app, db)
     
-    api = Api(app)
+    
 
     api.add_namespace(ons)
     api.add_namespace(ans, path="/auth")
 
+    
+    
     @app.shell_context_processor
     def make_shell_context():
         return {
