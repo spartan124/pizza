ef93cfb04c265a5d26c77f86f42710ab70649ab6
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-02-16 09:29:34 +0100

    added db update and delete functions

--- api/models/orders.py
+++ api/models/orders.py
@@ -33,6 +33,13 @@ class Order(db.Model):
     def save(self):
         db.session.add(self)
         db.session.commit()
+        
+    def update(self):
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
     
     @classmethod
     def get_by_id(cls, id):
