9ef636894006581e8fd646f29c4cadbbb00210b7
Ojonuba Jeremiah <ojonuba93@gmail.com>
2023-02-25 00:53:11 +0100

    minor mod

--- api/__init__.py
+++ api/__init__.py
@@ -1,16 +1,19 @@
+import pathlib
+
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended.exceptions import JWTExtendedException
 from flask_migrate import Migrate
 from flask_restx import Api
-from werkzeug.exceptions import NotFound, MethodNotAllowed
+from werkzeug.exceptions import MethodNotAllowed, NotFound
+
 from .auth.views import auth_namespace as ans
 from .config.config import config_dict
 from .models.orders import Order
 from .models.users import User
 from .orders.views import order_namespace as ons
 from .utils import db
-from flask_jwt_extended.exceptions import JWTExtendedException
-import pathlib
+
 
 def create_app(config=config_dict['dev']):
     app = Flask(__name__)
@@ -20,18 +23,33 @@ def create_app(config=config_dict['dev']):
 
     
     db.init_app(app)
-    
-    api = Api(app)
-
     jwt = JWTManager()
+    jwt.init_app(app)
+
+    migrate = Migrate(app, db)
+    authorizations= {
+        "Bearer Auth": {
+            'type': 'apiKey',
+            'in':'header',
+            'name':'Authorization',
+            'description': 'Add a JWT token to the header with ** Bearer &lt;JWT&gt; token to authorize **'
+        }
+    }
+
+    api = Api(app,
+              title='Pizza Delivery API',
+              description='A simple pizza delivery REST API service',
+              authorizations=authorizations,
+              security='Bearer Auth'
+              )
+    
+  
     
     @api.errorhandler(JWTExtendedException)
     def handle_jwt_exceptions(error):
         return {'message': str(error)}, getattr(error, 'code', 401)
     
-    jwt.init_app(app)
-
-    migrate = Migrate(app, db)
+    
     
     
 
